b3d7da38c43672758536dc79705aeeb7ce8c4f9d
Siddesh <Siddesh@DESKTOP-5H6SQA8>
2021-12-19 16:03:48 -0800

    Add project files.

---
+++ WebApplication1/Controllers/UserDetailsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationAssignment.Models;
+
+namespace WebApplicationAssignment.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public static List<Customer> customer = new List<Customer>();
+        static int Id = 0;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
+        {
+            ViewData["TotalCustomers"] = customer.Count();
+            return View(customer);
+        }
+
+        [HttpGet]
+        [Route("Create")]
+        public IActionResult Create()
+        {
+
+            ViewBag.ID = ++Id;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public IActionResult Create(Customer cust)
+        {
+            customer.Add(cust);
+            return RedirectToAction("Get", customer);
+        }
+
+        [HttpGet]
+        [Route("Edit")]
+        public IActionResult Edit(int id)
+        {
+            Customer oCustomer = customer.Where(a => a.Id.Equals(id)).Select(s => s).FirstOrDefault();
+            return View(oCustomer);
+        }
+
+
+        [HttpPost]
+        [Route("Edit")]
+        public IActionResult Edit(Customer cust)
+        {
+            Customer oCustomer = customer.Where(a => a.Id.Equals(cust.Id)).Select(s => s).FirstOrDefault();
+            if (oCustomer != null)
+            {
+                oCustomer.Name = cust.Name;
+                oCustomer.Age = cust.Age;
+                oCustomer.Gender = cust.Gender;
+            }
+            return RedirectToAction("Get", oCustomer);
+        }
+
+        [HttpGet]
+        [Route("Details")]
+        public IActionResult Details(int id)
+        {
+            Customer cust = customer.Where(a => a.Id == id).Select(s => s).FirstOrDefault();
+            return View(cust);
+        }
+
+        [Route("Delete")]
+        public ActionResult Delete(int id)
+        {
+            Customer cust = customer.Find(c => c.Id == id);
+            customer.Remove(cust);
+            return RedirectToAction("Get");
+        }
+    }
+}
---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationAssignment.Models
+{
+    public enum EGender
+    {
+        male = 1,
+        female = 2,
+        other = 3
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Name")]
+        public string Name { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide Age")]
+        public int Age { get; set; }
+
+
+        [Required(ErrorMessage = "Please Provide Gender")]
+        public EGender Gender { get; set; }
+    }
+}
